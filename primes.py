a44a605bb9bce09a137d53c3341090beb8f1b7d0
DubiousDSK <dilsadskaya@gmail.com>
2023-10-03 14:44:50 +0100

    Add the check for negative numbers & zero

--- primes.py
+++ primes.py
@@ -3,4 +3,21 @@
 
 def primes(number_of_primes):
     list = []
+    
+    if number_of_primes < 1:
+        raise ValueError
+    
+    i = 0
+    x = 2
+    while i < number_of_primes:
+        y = 1
+        counter = 0
+        while y <= x:
+            if (x % y) != 0:
+                counter += 1
+            y += 1
+        if counter == (x - 2):
+            list.append(x)
+            i += 1
+        x += 1
     return list
